7d1639e9c18e7ea6f97012a300e239e487664a97
vale <annamaria@DESKTOP-NQB56O3>
2023-11-29 10:16:44 +0100

    Aggiunta icona alla GUI.

--- ProgettoSE_Gruppo16/src/progettose_gruppo16/ProgettoSE_Gruppo16.java
+++ ProgettoSE_Gruppo16/src/progettose_gruppo16/ProgettoSE_Gruppo16.java
@@ -8,6 +8,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -21,7 +22,8 @@ public class ProgettoSE_Gruppo16 extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        stage.setTitle("IFTTT");
+        stage.setTitle(" IFTTT ");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("image.png")));
         stage.setScene(scene);
         stage.show();
     }
