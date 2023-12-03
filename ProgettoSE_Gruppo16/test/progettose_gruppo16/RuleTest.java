079c4086776e68b4511f5a1b2fb5e68c144ccdce
vale <annamaria@DESKTOP-NQB56O3>
2023-12-03 12:22:51 +0100

    Modifica package.

---
+++ ProgettoSE_Gruppo16/test/progettose_gruppo16/RuleTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package progettose_gruppo16;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author valentina <your.name at your.org>
+ */
+public class RuleTest {
+    
+    public RuleTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
